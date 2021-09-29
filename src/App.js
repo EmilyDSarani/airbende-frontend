806b84689dae946e11f228e9b063ccfd5583a8a8
EmilyDSarani <emily.sarani@gmail.com>
2021-09-29 17:13:56 -0500

    Router set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// loosely based off my pokesearch lab, I just want to create this for fun
+import React, { Component } from 'react'
+import Home from "./Home.js"
+import Gaang from "./Gaang.js"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom"
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+        <header>
+          <NavLink exact activeClassName='active' to='/'>Home</NavLink>
+          <NavLink exact activeClassName='active' to='/Gaang'>Gaang</NavLink>
+        </header>
+        <Switch>
+          <Route
+          path="/"
+          exact
+          render={(routerProps) => <Home {...routerProps}/>}
+          />
+          <Route
+          path="/Gaang"
+          exact
+          render={(routerProps)=> <Gaang {...routerProps}/>}
+          />
+        </Switch>
+        </Router>
+      </>
+    )
+  }
 }
 
-export default App;
+
+
+
