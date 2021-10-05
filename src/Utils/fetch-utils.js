251de04a2e0ae6cd11e573deba651ed89a0edb89
EmilyDSarani <emily.sarani@gmail.com>
2021-10-05 17:12:21 -0500

    both get tests are working

--- src/Utils/fetch-utils.js
+++ src/Utils/fetch-utils.js
@@ -1,8 +1,14 @@
 import request from "superagent";
 
 
-export async function getAllAvatar (){
-    const response = await request.get('https://airbendercharacters.herokuapp.com/avatar')
+export async function getAllAvatar(){
+    const response = await request.get(`https://airbendercharacters.herokuapp.com/avatar`)
 
     return response.body; 
+}
+
+export async function getOneAvatar(id){
+    const response = await request.get(`https://airbendercharacters.herokuapp.com/avatar/${id}`)
+
+    return response.body;
 }
--- src/Utils/fetch-utils.test.js
+++ src/Utils/fetch-utils.test.js
@@ -1,5 +1,6 @@
 import 
 {getAllAvatar,
+getOneAvatar,
 
 } from './fetch-utils.js';
 
@@ -17,3 +18,18 @@ test('get all avatar characters', async () => {
 
     expect(response[0]).toEqual(expectations)
 });
+
+test('get one avatar character', async () =>{
+    const expectations = {
+        id: 3,
+        name: expect.any(String),
+        element_id: expect.any(Number),
+        element_name: expect.any(String),
+        title: expect.any(String),
+        img: expect.any(String),
+        owner_id:1
+    }
+    const response = await getOneAvatar(3);
+
+    expect(response).toEqual(expectations)
+})
