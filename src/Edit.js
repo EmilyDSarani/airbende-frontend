4830695c5e6eeccc91d32f8bcaac4eec861647e7
EmilyDSarani <emily.sarani@gmail.com>
2021-10-06 17:31:14 -0500

    got distracted helping someone else, dont know what I need to commit

--- src/Edit.js
+++ src/Edit.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import DelIntro from './UserFill/DelIntro'
 import UserEdit from './UserFill/UserEdit'
 
 
 export default class Edit extends Component {
     render() {
-
+//In the UserEdit, it is allowing me to render in the App.js to go to the Edit page and pull of up USEREDIT. The UserEdit is where all the meat is happening and spreading it allows it to come in rendered correctly. 
         return (
             <div>
+            <DelIntro />
              <UserEdit {...this.props} />   
             </div>
         )
